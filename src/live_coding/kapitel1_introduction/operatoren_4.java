aab216107c254714aedde1405548a6b52f5824bb
Mohammad <orabe.mhd@gmail.com>
2025-04-25 22:04:26 +0200

    add operatoren

---
+++ src/live_coding/kapitel1_introduction/operatoren_4.java
@@ -0,0 +1,46 @@
+package live_coding.kapitel1_introduction;
+
+public class operatoren_4 {
+    public static void main(String[] args) {
+
+
+    // Operatoren
+    // 1. Arithemeitsch
+    // + - * /
+
+    // schreibe ein Program, welches die flaeche eines Rechteck berechnet.
+    int laenge = 5;
+
+        System.out.println(laenge);
+
+    int breite = 2;
+
+    int flaeche = laenge * breite;
+        System.out.println(flaeche); // 10
+
+    int umfang = 2 * (laenge + breite);
+        System.out.println(umfang); // 14
+
+
+    // deklarieren und intialiseren auf einmal
+    int x = 5;
+        System.out.println(x); // 5
+
+    // ueberschreiben
+    x =3;
+    System.out.println(x); // 3
+
+    int y; // deklarieren (definieren)
+
+    // initialisieren: zum ersten mal einen Wert geben / einen Wert zuweisen (geben)
+    y =2;
+    System.out.println(y); // 2
+
+    // ueberschreiben: den alten Wert mit einem neuen ersezten / einen Wert zuweisen (geben)
+    y =7;
+    System.out.println(y); // 7
+
+
+    }
+
+}
--- src/live_coding/kapitel1_introduction/variablen_und_literale_3.java
+++ src/live_coding/kapitel1_introduction/variablen_und_literale_3.java
@@ -16,22 +16,69 @@ public class variablen_und_literale_3 {
     */
 
         // datentype: int
-        // name der variable (Bezeichner): x
+        // name der variable (Bezeichner): a
         // inhalt der varible (Literal): 3
-         int x = 3;
+         int a = 3;
 
-        double y = 4.2;
+        double b = 4.2;
         int ahmad_und_khaled = 5;
         String v = "hallo";
-        char z = 'Q';
+        char c = 'Q';
 
         System.out.println(ahmad_und_khaled);
         System.out.println("ahmad");
-        System.out.println(x);
-        System.out.println(x);
-        System.out.println(x);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
 
-        System.out.println("x");
+        System.out.println("a");
+
+
+
+        // ; simikolon
+        // " " anfuehrungszeichen
+
+        System.out.println("Ich liebe java");
+        System.out.println("Ich liebe programmieren");
+
+        // Variablen
+        // 1. Datentyp der Varibale
+        // 2. name der Variable (Bezeichner)
+        // 3. Inhalt der Variable (Literal)
+
+        // Syntax: grammatic
+        // Konvention: abmachung unter Programmierer
+
+        int meinAlterNaechstesJahr = 20;
+        double gewicht = 41.3;
+        String name = "Ali";
+        char landaer = 'D';
+
+        System.out.println("name");
+        System.out.println(meinAlterNaechstesJahr);
+        System.out.println(landaer);
+        System.out.println(landaer);
+        System.out.println(landaer);
+        System.out.println(landaer);
+
+
+        // T-shirt preis  20,99 cents
+        float tshirtPreis = 20.99f;
+
+        // erstelle eine variable
+        // diese variable beschreibt
+        // die PLZ (postleitzahl)-
+        // ZIP Code einer Stadt.
+        short PLZ = 0345;
+
+        // int: -(2^31)-1 <= variable <= (2^31)-1
+        int views = 2147483647;
+
+        double x = 1.21343443453453;
+        float y  = 1.21343443453453f;
+
+        System.out.println(x);
+        System.out.println(y);
 
     }
 }
