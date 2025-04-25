238b8af93c666bef35cd239351303e6b3a401aca
Mohammad <orabe.mhd@gmail.com>
2025-04-25 15:02:31 +0200

    add resources

---
+++ src/live_coding/kapitel1_introduction/variablen_und_literale_3.java
@@ -0,0 +1,37 @@
+package live_coding.kapitel1_introduction;
+
+public class variablen_und_literale_3 {
+
+    public static void main(String[] args) {
+
+        // geschweifte Klammer, Schweifklammer
+
+
+    /*
+    System.out.println("hallo");
+    System.out.println("ich");
+    System.out.println("liebe");
+    System.out.println("java");
+    System.out.println("Hello World!");
+    */
+
+        // datentype: int
+        // name der variable (Bezeichner): x
+        // inhalt der varible (Literal): 3
+         int x = 3;
+
+        double y = 4.2;
+        int ahmad_und_khaled = 5;
+        String v = "hallo";
+        char z = 'Q';
+
+        System.out.println(ahmad_und_khaled);
+        System.out.println("ahmad");
+        System.out.println(x);
+        System.out.println(x);
+        System.out.println(x);
+
+        System.out.println("x");
+
+    }
+}
